874c8c4a63b8afd2beae5fffe98f728d585cc164
Kirill Golovin <golovin91k@gmail.com>
2025-04-30 22:34:14 +0300

    7

--- src/barter_system/barter_system/settings.py
+++ src/barter_system/barter_system/settings.py
@@ -10,7 +10,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY_DEFAULT')
-DEBUG = DEBUG = os.getenv('DEBUG', 'True') == 'True'
+# DEBUG = DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS', '127.0.0.1')).split()
 
