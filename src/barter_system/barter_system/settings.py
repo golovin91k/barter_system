5c2db0f987b7a98d072d9ee864718b80538f884d
Kirill Golovin <golovin91k@gmail.com>
2025-04-30 23:03:21 +0300

    12

--- src/barter_system/barter_system/settings.py
+++ src/barter_system/barter_system/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
-###
+####
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
