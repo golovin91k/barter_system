f6573ecc2fb024779c052b2102fed75e0413441d
Kirill Golovin <golovin91k@gmail.com>
2025-04-30 23:16:40 +0300

    14

--- src/barter_system/ads/urls.py
+++ src/barter_system/ads/urls.py
@@ -5,30 +5,30 @@ from ads import views
 app_name = 'ads'
 
 urlpatterns = [
-    path('', views.AdsListView.as_view(), name='list'),
-    path('add_ad/', views.AdCreateView.as_view(), name='add_ad'),
-    path('ads/<int:ad_pk>/', views.AdDetailView.as_view(), name='detail_ad'),
-    path('ads/<int:ad_pk>/edit/', views.AdUpdateView.as_view(), name='update_ad'),
+    path('barter_system/', views.AdsListView.as_view(), name='list'),
+    path('barter_system/add_ad/', views.AdCreateView.as_view(), name='add_ad'),
+    path('barter_system/ads/<int:ad_pk>/', views.AdDetailView.as_view(), name='detail_ad'),
+    path('barter_system/ads/<int:ad_pk>/edit/', views.AdUpdateView.as_view(), name='update_ad'),
     path(
-        'ads/<int:ad_pk>/delete/', views.AdDeleteView.as_view(), name='delete_ad'),
+        'barter_system/ads/<int:ad_pk>/delete/', views.AdDeleteView.as_view(), name='delete_ad'),
 
     path(
-        'ads/<int:ad_receiver_pk>/user-available-ads/',
+        'barter_system/ads/<int:ad_receiver_pk>/user-available-ads/',
         views.UserAvailableAdsView.as_view(),
         name='user_available_ads'),
     path(
-        'ads/<int:ad_receiver_pk>/user-available-ads/<int:ad_sender_pk>/',
+        'barter_system/ads/<int:ad_receiver_pk>/user-available-ads/<int:ad_sender_pk>/',
         views.ExcCreateView.as_view(),
         name='add_exc'),
     path(
-        'excs/<int:exc_pk>/edit/', views.ExcUpdateView.as_view(),
+        'barter_system/excs/<int:exc_pk>/edit/', views.ExcUpdateView.as_view(),
         name='update_exc'),
     path(
-        'excs/<int:exc_pk>/delete/', views.ExcDeleteView.as_view(),
+        'barter_system/excs/<int:exc_pk>/delete/', views.ExcDeleteView.as_view(),
         name='delete_exc'),
 
-    path('user-ads/', views.UserAdsView.as_view(), name='user_ads'),
-    path('user-excs/', views.UserExcsView.as_view(), name='user_excs'),
+    path('barter_system/user-ads/', views.UserAdsView.as_view(), name='user_ads'),
+    path('barter_system/user-excs/', views.UserExcsView.as_view(), name='user_excs'),
 
     # path('error/', views.Error.as_view(), name='error')
     ]
